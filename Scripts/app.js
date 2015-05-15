2f37d3ffb54efe48036f44f1daafd0ccb0498b82
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2015-05-15 13:08:02 +0300

    Add cache for state persistence

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,16 +2,32 @@
     baseUrl: "scripts"
 })
 
-define(["fileIO", "filters"], function (FileIO, Filters) {
+define(["fileIO", "filters", 'cache/cache'], function (FileIO, Filters, Cache) {
     // App logic
+
+    //#region Helpers
 
     function applyFilter(command) {
         Filters[command](canvas);
+        Cache.store();
     }
 
+    function initCache() {
+        Cache.init(canvas);
+    }
+
+    //#endregion
+
+
+    //#region File IO
+
     loadFile.onchange = function () {
-        FileIO.loadFile(this, canvas);
+        FileIO.loadFile(this, canvas, initCache);
     };
+
+    //#endregion
+
+    //#region Filters
 
     invertButton.onclick = function () {
         applyFilter("invert");
@@ -20,5 +36,24 @@ define(["fileIO", "filters"], function (FileIO, Filters) {
     grayscaleButton.onclick = function () {
         applyFilter("grayscale");
     };
+
+    //#endregion
+
+
+    //#region Cache
+
+    undoButton.onclick = function () {
+        Cache.undo();
+    };
+
+    redoButton.onclick = function () {
+        Cache.redo();
+    };
+
+    resetButton.onclick = function () {
+        Cache.reset();
+    };
+
+    //#endregion
 
 });
---
+++ Scripts/cache/cache.js
@@ -0,0 +1,85 @@
+﻿define(function () {
+    var cacheArray = [];
+    var original = {};
+    var capacity = 10;
+    var canvas = null;
+    var currentIndex = 0;
+    var lastIndex = 0;
+
+    function getImageData() {
+        var context = canvas.getContext("2d");
+        var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+        return imageData;
+    }
+
+    function apply(imageData) {
+        // TODO width and height
+        var context = canvas.getContext("2d");
+
+        context.putImageData(imageData, 0, 0);
+    }
+
+    function initArray(data) {
+        currentIndex = lastIndex = 0;
+
+        cacheArray = [];
+
+        cacheArray[currentIndex] = data;
+    }
+
+    function init(cv) {
+        canvas = cv;
+        original = getImageData(cv);
+
+        initArray(original);
+    }
+
+    function store() {
+        var imageData = getImageData(canvas);
+
+        if (currentIndex === capacity - 1) {
+            cacheArray = cacheArray.slice(1);
+        } else {
+            currentIndex = lastIndex = currentIndex + 1;
+        }
+
+        cacheArray[currentIndex] = imageData;
+    }
+
+    function reset() {
+        apply(original);
+
+        initArray(original);
+    }
+
+    function undo() {
+        var newIndex = currentIndex - 1;
+
+        if (newIndex < 0) {
+            return;
+        }
+
+        currentIndex = newIndex;
+        apply(cacheArray[currentIndex]);
+    }
+
+    function redo() {
+        var newIndex = currentIndex + 1;
+
+        if (newIndex > lastIndex) {
+            return;
+        }
+
+        currentIndex = newIndex;
+        apply(cacheArray[currentIndex]);
+    }
+
+    return {
+        init: init,
+        reset: reset,
+        store: store,
+        undo: undo,
+        redo: redo
+    };
+});
