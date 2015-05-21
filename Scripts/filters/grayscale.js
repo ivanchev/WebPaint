ebf7a01dc927dbfdb6a5287fe99779cb857425b6
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2015-05-21 12:25:02 +0300

    Change grayscale method to weighted

--- Scripts/filters/grayscale.js
+++ Scripts/filters/grayscale.js
@@ -4,7 +4,7 @@
             var average;
 
             for (var i = 0, l = data.length; i < l; i += 4) {
-                average = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                average = 0.2126 * data[i] + 0.7152 * data[i + 1] + 0.0722 * data[i + 2];
 
                 data[i] = average;
                 data[i + 1] = average;
